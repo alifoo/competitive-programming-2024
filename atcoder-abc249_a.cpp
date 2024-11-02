97f0b904733ff0ca2446c6b8282404a35220e82d
alifoo <alissonayres.trb@gmail.com>
2024-11-02 18:41:02 -0300

    new solution for jogging

--- atcoder-abc249_a.cpp
+++ atcoder-abc249_a.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int main() {
-  int a,b,c,d,e,f,x,ta,ao;
+  int a,b,c,d,e,f,x,takahashi_left,aoki_left;
   int aoki, takahashi;
 
   cin >> a >> b >> c >> d >> e >> f >> x;
   
-  ta = x - (a + c);
-  if(ta>0){
-    takahashi = (a + ta) * c;
+  takahashi_left = x - (a + c);
+  if(takahashi_left>0){
+    takahashi = (a + takahashi_left) * b;
   }else{
-    takahashi = a * c;
+    takahashi = a * b;
   }
   
-  ao = x - (d + f);
-  if(ao>0){
-    aoki = (e + ao) * d;
+  aoki_left = x - (d + f);
+  if(aoki_left>0){
+    aoki = (d + aoki_left) * e;
   }else{
-    aoki = e * f;
+    aoki = d * e;
   }
 
   if(takahashi>aoki) {
